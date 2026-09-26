5e0a43be5add1ea578a9d0e389f8f13bb5d04177
mperezj01 <mperezj01@informatica.iesvalledeljerteplasencia.es>
2026-09-26 11:31:43 +0200

    Ejercicios terminados
    Añadidos ejercicios

--- TEMA2/Ejercicio23-Tema2/src/ejercicio23/tema2/Ejercicio23Tema2.java
+++ TEMA2/Ejercicio23-Tema2/src/ejercicio23/tema2/Ejercicio23Tema2.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio23.tema2;
-import java.util.Scanner;
+import java.util.Scanner;//Importo Scanner del paquete java.util
 
 /**
  *
@@ -15,18 +15,18 @@ public class Ejercicio23Tema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int unidades;
-        float precio, totalCompra;
+        int unidades;//Declaro la variable unidades como número entero
+        float precio, totalCompra;//Declaro las variables precio y totalCompra como float
         
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Por favor, introduzca el precio del modelo de ordenador que desea comprar: ");
-        precio=entrada.nextFloat();
+        Scanner entrada = new Scanner(System.in);//Añado la variable de entrada que me va a pedir la consola con el Sout
+        System.out.println("Por favor, introduzca el precio del modelo de ordenador que desea comprar: ");//Pido el precio
+        precio=entrada.nextFloat();//Asigno el valor de entrada a la variable precio
         
-        System.out.println("¿Cuantas unidades quiere llevarse? ");
-        unidades=entrada.nextInt();
+        System.out.println("¿Cuantas unidades quiere llevarse? ");//Pido el número de unidades
+        unidades=entrada.nextInt();//Asigno el valor de entrada a la variable unidades
         
-        totalCompra=precio*unidades;
-        System.out.println("El precio total de su compra es de "+totalCompra+" euros.");
+        totalCompra=precio*unidades;//Asigno el producto de las variables precio y unidades a la variable totalCompra
+        System.out.println("El precio total de su compra es de "+totalCompra+" euros.");//Imprimo en pantalla el resultado
         // TODO code application logic here
     }
     
--- TEMA2/Ejercicio9_tema2/src/ejercicio9_tema2/Ejercicio9_tema2.java
+++ TEMA2/Ejercicio9_tema2/src/ejercicio9_tema2/Ejercicio9_tema2.java
@@ -9,11 +9,18 @@ package ejercicio9_tema2;
  * @author alumno
  */
 public class Ejercicio9_tema2 {
+    final static double PI = 3.14159;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        double radio, longitud;
+        radio=3.55;
+        longitud=2*PI*radio;
+        
+        System.out.println("La longitud de una circunferencia cuyo radio vale 3.55 metros sería igual a: "+longitud+" metros");
+        
         // TODO code application logic here
     }
     
